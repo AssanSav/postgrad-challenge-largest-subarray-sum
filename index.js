51422dc46a5bc298b68c41f0fa55e04286ba07ec
AssanSav <assane.savadogo81@gmail.com>
2020-07-10 22:16:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-let currentSum = 0;
-let largestSum = 0;
 
 const largestSubarraySum = (array) => {
+  let currentSum = 0;
+  let largestSum = 0;
   for (let number of array) {
     currentSum = Math.max(0, (currentSum + number));
     largestSum = Math.max(largestSum, currentSum)
